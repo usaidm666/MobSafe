bf93c6ea50acdf19ee86be022a8c6868cfb5d3f8
superpoussin22 <vincent.nadal@orange.fr>
2023-05-02 14:20:08 +0200

    move to Google Smali fork

--- mobsf/StaticAnalyzer/views/android/converter.py
+++ mobsf/StaticAnalyzer/views/android/converter.py
@@ -43,7 +43,7 @@ def dex_2_smali(app_dir, tools_dir):
                         and is_file_exists(settings.BACKSMALI_BINARY)):
                     bs_path = settings.BACKSMALI_BINARY
                 else:
-                    bs_path = os.path.join(tools_dir, 'baksmali-2.5.2.jar')
+                    bs_path = os.path.join(tools_dir, 'baksmali-3.0.2.jar')
                 output = os.path.join(app_dir, 'smali_source/')
                 smali = [
                     find_java_binary(),
